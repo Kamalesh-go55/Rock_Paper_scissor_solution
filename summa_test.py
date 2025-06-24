c024c595ad583fff1ab36780454a49e1067df055
Kamalesh Kannan <kamalesh555go@gmail.com>
2025-06-24 14:03:34 +0530

    Add files via upload
    full_game_rock_paper_scissor

---
+++ summa_test.py
@@ -0,0 +1,112 @@
+import random
+
+
+def play(player1, player2, num_games, verbose=False):
+    p1_prev_play = ""
+    p2_prev_play = ""
+    results = {"p1": 0, "p2": 0, "tie": 0}
+
+    for _ in range(num_games):
+        p1_play = player1(p2_prev_play)
+        p2_play = player2(p1_prev_play)
+
+        if p1_play == p2_play:
+            results["tie"] += 1
+            winner = "Tie."
+        elif (p1_play == "P" and p2_play == "R") or (
+                p1_play == "R" and p2_play == "S") or (p1_play == "S"
+                                                       and p2_play == "P"):
+            results["p1"] += 1
+            winner = "Player 1 wins."
+        elif p2_play == "P" and p1_play == "R" or p2_play == "R" and p1_play == "S" or p2_play == "S" and p1_play == "P":
+            results["p2"] += 1
+            winner = "Player 2 wins."
+
+        if verbose:
+            print("Player 1:", p1_play, "| Player 2:", p2_play)
+            print(winner)
+            print()
+
+        p1_prev_play = p1_play
+        p2_prev_play = p2_play
+
+    games_won = results['p2'] + results['p1']
+
+    if games_won == 0:
+        win_rate = 0
+    else:
+        win_rate = results['p1'] / games_won * 100
+
+    print("Final results:", results)
+    print(f"Player 1 win rate: {win_rate}%")
+
+    return (win_rate)
+
+def abbey(prev_opponent_play,
+          opponent_history=[],
+          play_order=[{
+              "RR": 0,
+              "RP": 0,
+              "RS": 0,
+              "PR": 0,
+              "PP": 0,
+              "PS": 0,
+              "SR": 0,
+              "SP": 0,
+              "SS": 0,
+          }]):
+
+    if not prev_opponent_play:
+        prev_opponent_play = 'R'
+    opponent_history.append(prev_opponent_play)
+
+    last_two = "".join(opponent_history[-2:])
+    if len(last_two) == 2:
+        play_order[0][last_two] += 1
+
+    potential_plays = [
+        prev_opponent_play + "R",
+        prev_opponent_play + "P",
+        prev_opponent_play + "S",
+    ]
+
+    sub_order = {
+        k: play_order[0][k]
+        for k in potential_plays if k in play_order[0]
+    }
+
+    prediction = max(sub_order, key=sub_order.get)[-1:]
+
+    ideal_response = {'P': 'S', 'R': 'P', 'S': 'R'}
+    return ideal_response[prediction]
+
+
+def player(prev_play, opponent_history=[], play_order={}):
+    if not prev_play:
+        prev_play = 'R'
+
+    opponent_history.append(prev_play)
+    prediction = 'P'
+
+    if len(opponent_history) > 2:
+        last_five = "".join(opponent_history[-3:])
+        play_order[last_five] = play_order.get(last_five, 0) + 1
+        
+        potential_plays = [
+            "".join([*opponent_history[-2:], v]) 
+            for v in ['R', 'P', 'S']
+        ]
+
+        sub_order = {
+            k: play_order[k]
+            for k in potential_plays if k in play_order
+        }
+
+        if sub_order:
+            prediction = max(sub_order, key=sub_order.get)[-1:]
+
+    ideal_response = {'P': 'S', 'R': 'P', 'S': 'R'}
+
+    return ideal_response[prediction]
+
+play(player, abbey, 1000)
